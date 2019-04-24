1a34de4d406e9c30555c993076a2a675edabbe21
SpyderTL <j_bair@bellsouth.net>
2019-04-24 00:58:43 -0500

    Added documentation.

--- StarFoxBrowser/Nodes/StarFoxLevel.cs
+++ StarFoxBrowser/Nodes/StarFoxLevel.cs
@@ -32,18 +32,20 @@ namespace StarFoxBrowser.Nodes
 					{
 						case 0x00:
 							// 3D Object (16-bit vector) (8-bit lookup table index)
-							var z = reader.ReadInt16();
-							var x = reader.ReadInt16();
-							var y = reader.ReadInt16();
-							var timer = reader.ReadUInt16();
-							var objectNumber = reader.ReadByte();		// Lookup object address in table at 0x2848
-							var end = reader.ReadByte();
+							reader.ReadBytes(10);
+							//	var z = reader.ReadInt16();
+							//	var x = reader.ReadInt16();
+							//	var y = reader.ReadInt16();
+							//	var timer = reader.ReadUInt16();
+							//	var objectNumber = reader.ReadByte();       // Lookup object address in table at 0x2848
+							//	var end = reader.ReadByte();
+							//	var unknown = reader.ReadBytes(1);
 
 							Nodes.Add("00 - 3D Object");
 							break;
 
-						case 0x01:
-							break;
+						//case 0x01:
+						//	break;
 
 						case 0x02:
 							// End Level
@@ -52,17 +54,17 @@ namespace StarFoxBrowser.Nodes
 
 						case 0x04:
 							// Loop Segment
-							var unknown = reader.ReadBytes(4);
+							reader.ReadBytes(4);
 							Nodes.Add("04 - Loop Segment");
 							break;
 
-						case 0x06:
-							unknown = reader.ReadBytes(11);
-							Nodes.Add("06 - Unknown");
-							break;
+						//case 0x06:
+						//	unknown = reader.ReadBytes(11);
+						//	Nodes.Add("06 - Unknown");
+						//	break;
 
 						case 0x0a:
-							unknown = reader.ReadBytes(15);
+							reader.ReadBytes(15);
 							Nodes.Add("0a - Unknown");
 							break;
 
@@ -73,25 +75,25 @@ namespace StarFoxBrowser.Nodes
 
 						case 0x10:
 							// Initialize Level
-							unknown = reader.ReadBytes(2);
+							reader.ReadBytes(2);
 							Nodes.Add("10 - Initialize Level");
 							break;
 
 						case 0x12:
 							// Z Timer (16-bit)
-							timer = reader.ReadUInt16();
+							reader.ReadUInt16();
 							Nodes.Add("12 - Z-Timer");
 							break;
 
 						case 0x14:
 							// Change Music
-							unknown = reader.ReadBytes(1);
+							reader.ReadBytes(1);
 							Nodes.Add("14 - Change Music");
 							break;
 
 						case 0x28:
 							// Warp w/ Return
-							unknown = reader.ReadBytes(3);
+							reader.ReadBytes(3);
 							//unknown = reader.ReadBytes(0x24);
 							Nodes.Add("28 - Warp and Return");
 							break;
@@ -101,46 +103,46 @@ namespace StarFoxBrowser.Nodes
 							Nodes.Add("2a - Return From Warp");
 							break;
 
-						case 0x2b:
-							unknown = reader.ReadBytes(0x24);
-							Nodes.Add("2b - Unknown");
-							break;
+						//case 0x2b:
+						//	unknown = reader.ReadBytes(0x24);
+						//	Nodes.Add("2b - Unknown");
+						//	break;
 
 						case 0x2c:
 							// Warp If Condition
-							unknown = reader.ReadBytes(5);
+							reader.ReadBytes(5);
 							//unknown = reader.ReadBytes(10);
 							Nodes.Add("2c - Warp If True");
 							break;
 
-						case 0x2d:
-							break;
+						//case 0x2d:
+						//	break;
 
 						case 0x2e:
 							// Warp
-							unknown = reader.ReadBytes(3);
+							reader.ReadBytes(3);
 							Nodes.Add("2e - Warp");
 							break;
 
 						case 0x36:
 							// Rotate Previous Object
-							unknown = reader.ReadBytes(3);
+							reader.ReadBytes(3);
 							Nodes.Add("36 - Rotate Previous Object");
 							break;
 
-						case 0x38:
-							unknown = reader.ReadBytes(11);
-							break;
+						//case 0x38:
+						//	unknown = reader.ReadBytes(11);
+						//	break;
 
-						case 0x3a:
-							break;
+						//case 0x3a:
+						//	break;
 
-						case 0x3c:
-							unknown = reader.ReadBytes(2);
-							break;
+						//case 0x3c:
+						//	unknown = reader.ReadBytes(2);
+						//	break;
 
-						case 0x3e:
-							break;
+						//case 0x3e:
+						//	break;
 
 						case 0x44:
 							// Fade Out
@@ -148,73 +150,75 @@ namespace StarFoxBrowser.Nodes
 							break;
 
 						case 0x4e:
+							Nodes.Add("4e - Unknown");
 							break;
 
-						case 0x50:
-							unknown = reader.ReadBytes(7);
-							Nodes.Add("50 - Unknown");
-							break;
+						//case 0x50:
+						//	unknown = reader.ReadBytes(7);
+						//	Nodes.Add("50 - Unknown");
+						//	break;
 
 						case 0x5a:
+							Nodes.Add("5a - Palette Change Previous Object");
+							break;
+
+						case 0x5c:
+							//unknown = reader.ReadBytes(4);
+							//unknown = reader.ReadBytes(8);
+							//unknown = reader.ReadBytes(18);
+							//unknown = reader.ReadBytes(38);
+							reader.ReadBytes(18);
+							Nodes.Add("5c - Unknown");
 							break;
 
 						case 0x5e:
 							// Screen Transition
-							unknown = reader.ReadBytes(5);
+							reader.ReadBytes(5);
 							Nodes.Add("5e - Screen Transition");
-							break;
-
-						case 0x5c:
-							unknown = reader.ReadBytes(23);
-							Nodes.Add("5c - Unknown");
 							break;
 
 						case 0x64:
 							// Reset Camera
-							unknown = reader.ReadBytes(1);
+							reader.ReadBytes(1);
 							Nodes.Add("64 - Reset Camera");
-							break;
-
-						case 0x66:
-							// Reset Camera 2
-							Nodes.Add("66 - Reset Camera 2");
 							break;
 
 						case 0x70:
 							// 3D Object (8-bit vector) (8-bit lookup table index)
-							//unknown = reader.ReadBytes(6);
-							z = reader.ReadByte();
-							x = reader.ReadSByte();
-							y = reader.ReadSByte();
-							timer = reader.ReadUInt16();
-							objectNumber = reader.ReadByte();
-							var behavior = reader.ReadByte();
+							reader.ReadBytes(6);
+							//	z = reader.ReadByte();
+							//	x = reader.ReadSByte();
+							//	y = reader.ReadSByte();
+							//	timer = reader.ReadUInt16();
+							//	objectNumber = reader.ReadByte();
+							//	//var behavior = reader.ReadByte();
 							Nodes.Add("70 - 3D Object");
 							break;
 
 						case 0x74:
 							// 3D Object Behavior (16-bit vector) (8-bit table lookup index)
-							z = reader.ReadInt16();
-							x = reader.ReadInt16();
-							y = reader.ReadInt16();
-							timer = reader.ReadUInt16();
-							behavior = reader.ReadByte();               // TODO: Find lookup table
+							reader.ReadBytes(9);
+							//	z = reader.ReadInt16();
+							//	x = reader.ReadInt16();
+							//	y = reader.ReadInt16();
+							//	timer = reader.ReadUInt16();
+							//	var behavior = reader.ReadByte();               // TODO: Find lookup table
 							Nodes.Add("74 - 3D Object");
 							break;
 
 						case 0x76:
 							// 3D Object (16-bit vector)
-							unknown = reader.ReadBytes(5);
-							//unknown = reader.ReadBytes(10);
-							//z = reader.ReadByte();
-							//x = reader.ReadSByte();
-							//y = reader.ReadSByte();
-							//timer = reader.ReadUInt16();
-							//var objectAddress = reader.ReadUInt16();
-							//var behavior2 = reader.ReadBytes(4);
+							reader.ReadBytes(5);
+							//	//unknown = reader.ReadBytes(10);
+							//	//z = reader.ReadByte();
+							//	//x = reader.ReadSByte();
+							//	//y = reader.ReadSByte();
+							//	//timer = reader.ReadUInt16();
+							//	//var objectAddress = reader.ReadUInt16();
+							//	//var behavior2 = reader.ReadBytes(4);
 
-							//var color = reader.ReadByte();
-							//end = reader.ReadByte();
+							//	//var color = reader.ReadByte();
+							//	//end = reader.ReadByte();
 							Nodes.Add("76 - 3D Object");
 							break;
 
@@ -225,11 +229,15 @@ namespace StarFoxBrowser.Nodes
 							Nodes.Add("78 - Native Code");
 							break;
 
-						case 0x82:
-							// Unknown
-							unknown = reader.ReadBytes(1);
-							Nodes.Add("82 - Unknown");
+						case 0x7a:
+							Nodes.Add("7a - Unknown");
 							break;
+
+						//case 0x82:
+						//	// Unknown
+						//	unknown = reader.ReadBytes(1);
+						//	Nodes.Add("82 - Unknown");
+						//	break;
 
 						case 0x84:
 							// Change Palette
@@ -238,60 +246,60 @@ namespace StarFoxBrowser.Nodes
 
 						case 0x86:
 							// 3D Object (16-bit vector) (16-bit address)
-							//reader.ReadBytes(13);
-							z = reader.ReadInt16();
-							x = reader.ReadInt16();
-							y = reader.ReadInt16();
-							timer = reader.ReadUInt16();
-							var objectAddress = reader.ReadUInt16();
-							var behavior2 = reader.ReadBytes(4);
+							reader.ReadBytes(13);
+							//	z = reader.ReadInt16();
+							//	x = reader.ReadInt16();
+							//	y = reader.ReadInt16();
+							//	timer = reader.ReadUInt16();
+							//	var objectAddress = reader.ReadUInt16();
+							//	var behavior2 = reader.ReadBytes(4);
 
-							var color = reader.ReadByte();
-							end = reader.ReadByte();
+							//	var color = reader.ReadByte();
+							//	end = reader.ReadByte();
 							Nodes.Add("86 - 3D Object");
 							break;
 
 						case 0x8a:
 							// Z Timer (8-bit)
-							timer = reader.ReadByte();
+							reader.ReadByte();
 							Nodes.Add("8a - Z-Timer");
 							break;
 
 						case 0x8c:
-							unknown = reader.ReadBytes(2);
+							reader.ReadBytes(2);
 							//unknown = reader.ReadBytes(4);
 							Nodes.Add("8c - Attach Behavior To Previous Object");
 							break;
 
-						case 0x9c:
-							unknown = reader.ReadBytes(6);
-							Nodes.Add("9c - Unknown");
-							break;
+						//case 0x9c:
+						//	unknown = reader.ReadBytes(6);
+						//	Nodes.Add("9c - Unknown");
+						//	break;
 
 						case 0xa2:
-							unknown = reader.ReadBytes(3);
+							reader.ReadBytes(3);
 							Nodes.Add("a2 - Unknown");
 							break;
 
-						case 0xb0:
-							unknown = reader.ReadBytes(7);
-							Nodes.Add("b0 - Unknown");
-							break;
+						//case 0xb0:
+						//	unknown = reader.ReadBytes(7);
+						//	Nodes.Add("b0 - Unknown");
+						//	break;
 
-						case 0xb9:
-							unknown = reader.ReadBytes(2);
-							Nodes.Add("b9 - Unknown");
-							break;
+						//case 0xb9:
+						//	unknown = reader.ReadBytes(2);
+						//	Nodes.Add("b9 - Unknown");
+						//	break;
 
-						case 0xba:
-							unknown = reader.ReadBytes(5);
-							Nodes.Add("ba - Unknown");
-							break;
+						//case 0xba:
+						//	unknown = reader.ReadBytes(5);
+						//	Nodes.Add("ba - Unknown");
+						//	break;
 
-						case 0xeb:
-							unknown = reader.ReadBytes(2);
-							Nodes.Add("eb - Unknown");
-							break;
+						//case 0xeb:
+						//	unknown = reader.ReadBytes(2);
+						//	Nodes.Add("eb - Unknown");
+						//	break;
 
 						default:
 							read = false;
--- StarFoxBrowser/Nodes/Usa10.cs
+++ StarFoxBrowser/Nodes/Usa10.cs
@@ -27,7 +27,7 @@ namespace StarFoxBrowser.Nodes
 			palettes.Nodes.Add(new StarFoxPalette { Text = "Night", Resource = Resource, Offset = 0x18aea });
 			palettes.Nodes.Add(new StarFoxPalette { Text = "Day", Resource = Resource, Offset = 0x18b0a });
 
-			palettes.Nodes.Add(new StarFoxPalette2 { Text = "Day", Resource = Resource, Offset = 0x90000 });
+			//palettes.Nodes.Add(new StarFoxPalette2 { Text = "3D", Resource = Resource, Offset = 0x90000 });
 
 			var models = new TreeNode("Models");
 
@@ -47,9 +47,58 @@ namespace StarFoxBrowser.Nodes
 			textures.Nodes.Add(new StarFoxTexture { Text = "Texture", Resource = Resource, Offset = 0x90000, Page = 1 });
 
 			var levels = new TreeNode("Levels");
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d068 });
 
 			levels.Nodes.Add(new StarFoxLevel { Text = "Training", Resource = Resource, Offset = 0x6eef8 });
-			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x68000 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x2b283 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x2b47f });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x2da11 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x2da42 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6b2b6 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6c8ae });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6c8ea });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d072 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d0d2 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d26d });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d2c8 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d30a });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d313 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d328 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d331 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d346 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d42a });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d58b });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d607 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d684 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6d729 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6dac6 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6db84 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6dbe1 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6dc97 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6dd70 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6ddd0 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6df51 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6dfac });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6dffb });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e01a });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e02f });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e08a });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e0fa });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e19b });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e3d9 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e439 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e5fb });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e658 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e6dd });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e7b2 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e821 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e8be });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e92e });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e966 });
+			levels.Nodes.Add(new StarFoxLevel { Text = "Corneria", Resource = Resource, Offset = 0x6d124 });
+			//levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6dd70 });
+
+			//levels.Nodes.Add(new StarFoxLevel { Text = "Level", Resource = Resource, Offset = 0x6e3cf });
 
 			Nodes.Add(palettes);
 			Nodes.Add(models);
